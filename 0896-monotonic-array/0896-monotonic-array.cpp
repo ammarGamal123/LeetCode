ea252ead224bfe363810bc8f5f3b5701bd1e1196
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-09-29 21:27:46 +0300

    Time: 113 ms (86.71%), Space: 96.7 MB (22.05%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     bool isMonotonic(vector<int>& a) {
-        if (a.size() == 1 || a.size() == 2)
+        if (is_sorted(a.begin() , a.end()) || is_sorted(a.rbegin() , a.rend()))
             return true;
-
-        bool ok = a[0] <= a[1];
-        bool ok2 = a[0] >= a[1];
-        for (int i = 1;i < a.size() - 1;i++){
-            ok &= a[i] <= a[i + 1];
-            ok2 &= a[i] >= a[i + 1];
-        }
-        return ok || ok2;
+        else return false;
     }
 };
