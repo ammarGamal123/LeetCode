9633a64c4595f237fc54fbdb3c923035514f98ce
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-10-06 15:40:19 +0300

    Time: 2 ms (49.96%), Space: 6.2 MB (73.89%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+       if (n <= 3) return n - 1;
+        
+        int prod = 1;
+        
+        while (n > 4)
+            prod *= 3 , n -= 3;
+        
+        prod *= n;
+        
+        return prod;
+    }
+};
