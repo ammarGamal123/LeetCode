c6370a1714483a94d43acf395f921f3ec5082a5c
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-11-09 22:43:17 +0200

    Time: 2 ms (40.74%), Space: 6.2 MB (75.37%) - LeetHub

--- 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.cpp
+++ 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
-    int Chebyshev(int sx, int sy, int fx, int fy){
-        return max(abs(sx-fx), abs(sy-fy) );
-    }
-    bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
-        int d=Chebyshev(sx, sy, fx, fy);
-        return d>0 ? t>=d: t!=1;
+    bool isReachableAtTime(int x1, int x2, int y1, int y2, int t) {
+        if (x1 == y1 && x2 == y2){
+            return (t > 1 || t == 0);
+        }
+        
+        int distRow = abs (y1 - x1);
+        int distColumn = abs (x2 - y2);
+        
+        int extraSquares = abs (distRow - distColumn);
+        
+         
+        return (min (distRow , distColumn) + extraSquares <= t ? true : false);
     }
 };
