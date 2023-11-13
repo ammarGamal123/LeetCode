0f0d9bd0a877c40d6325e0c22d91a91bc79cf383
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-11-13 22:28:08 +0200

    Time: 70 ms (14.30%), Space: 12.2 MB (82.19%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-    static bool checkVowels (char c){
-      return c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'O' || c == 'o'
-                || c == 'i' || c == 'I' || c == 'u' || c == 'U';
-    }
     
     string sortVowels(string s) {
+        int freq[70];
+        memset (freq, -1 , sizeof freq);
+        
+        freq['A' - 'A'] ++ , freq['E' - 'A'] ++ , freq['I' - 'A'] ++;
+        freq['O' - 'A'] ++ , freq['U' - 'A'] ++;
+        
+        freq['a' - 'A'] ++ , freq['e' - 'A'] ++ , freq['i' - 'A'] ++;
+        freq['o' - 'A'] ++ , freq['u' - 'A'] ++;
         
         priority_queue <char , vector <char> , greater <>> pq;
         
         for (int i = 0;i < s.size(); i++){
-            if (checkVowels(s[i]))
+            if (freq[s[i] - 'A'] >= 0)
                 pq.push(s[i]);
         }
         
         for (int i = 0;i < s.size(); i++){
-            if (checkVowels(s[i])){
+            if (freq[s[i] - 'A'] >= 0){
                 s[i] = pq.top();
                 pq.pop();
             }
