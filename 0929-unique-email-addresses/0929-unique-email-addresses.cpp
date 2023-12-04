915002b5db5493eeaafbf1178e4bbc0067fa9bdd
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-12-04 22:28:18 +0200

    Time: 24 ms (41.98%), Space: 13.8 MB (90.23%) - LeetHub

---
+++ 0929-unique-email-addresses/0929-unique-email-addresses.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int numUniqueEmails(vector<string>& s) {
+        int ans{};
+    map<string, set<string>> store;
+    
+    string after = "", before = "";
+    bool at = false, plus = false;
+
+    for (int i = 0; i < s.size(); i++) {
+        at = false , plus = false;
+        after = "", before = "";
+        for (int j = 0; j < s[i].size(); j++) {
+            if (s[i][j] == '@') {
+                at = true;
+                continue;
+            }
+            if (s[i][j] == '.' && !at) continue;
+            if (s[i][j] == '+' && !at) {
+                plus = true;
+                continue;
+            }
+            if (at){
+                after += s[i][j];
+            }
+            else if (!at && !plus){
+                before += s[i][j];
+            }
+
+        }
+
+        store[before].insert(after);
+    }
+    for (auto i : store){
+        ans += i.second.size();
+    }
+    return ans;
+    }
+};
