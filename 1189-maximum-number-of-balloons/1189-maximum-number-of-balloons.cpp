1ebba6977392f2a95e056b44ee72df4303ff378d
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-12-05 23:31:18 +0200

    Time: 4 ms (53.60%), Space: 6.9 MB (61.32%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-        int ans {INT_MAX};
+         int ans {INT_MAX};
         
-        map <char,int> mp;
+        vector <int> mp (26 , 0);
         
         for (char i : text)
-            mp[i] ++;
+            mp[i - 'a'] ++;
         
         short all = 0;
-        for (auto i : mp){
-            char cur = i.first;
+        for (int i = 0;i < mp.size(); i ++){
+            char cur = i + 97;
             if (cur == 'b'){
                 all ++;
-                ans = min (ans , i.second);
+                ans = min (ans , mp[i]);
             }
             else if (cur == 'a'){
                 all ++;
-                ans = min (ans , i.second);
+                ans = min (ans , mp[i]);
             }
-            else if (cur == 'l' && i.second >= 2){
+            else if (cur == 'l' && mp[i] >= 2){
                 all ++;
-                ans = min (ans , i.second / 2);
+                ans = min (ans , mp[i] / 2);
             }
-            else if (cur == 'o' && i.second >= 2){
+            else if (cur == 'o' && mp[i] >= 2){
                 all ++;
-                ans = min (ans , i.second / 2);
+                ans = min (ans , mp[i] / 2);
             }
             else if (cur == 'n'){
                 all ++;
-                ans = min (ans , i.second);
+                ans = min (ans , mp[i]);
             }
         }
         
