decd16b4015dd038bd460b054044145b1ab1e5d5
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-12-13 03:11:05 +0200

    Time: 17 ms (54.47%), Space: 14.3 MB (5.53%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -1,31 +1,6 @@
 class Solution {
 public:
-    
-   bool check (vector<vector<int>>&mat ,
-            vector<vector<int>>& revMat,
-            int i , int j){
-    int ones = 0;
-    for (int k = 0;k < mat[i].size(); k++){
-//        cout << mat[i][k] << " ";
-        ones += mat[i][k];
-    }
-    for (int k = 0; k < revMat[j].size(); k++){
-//        cout << revMat[j][k] << " ";
-        ones += revMat[j][k];
-    }
-    return ones == 2;
-
-//    cout << count(mat[i].begin() , mat[i].end() , 1) << endl;
-//    cout << count(revMat[j].begin() , mat[j].end() , 1) << endl;
-//
-//    if (count(mat[i].begin() , mat[i].end() , 1))
-//        return false;
-//    if (count(revMat[j].begin() , mat[j].end() , 1))
-//        return false;
-
-    return true;
-
-}
+ 
 int numSpecial(vector<vector<int>>& mat) {
     int ans {};
     int n = mat.size() , m = mat[0].size();
@@ -38,7 +13,15 @@ int numSpecial(vector<vector<int>>& mat) {
     for (int i = 0;i < mat.size(); i++){
         for (int j = 0;j < mat[i].size(); j ++){
             if (mat[i][j]){
-                ans += check (mat , revMat , i , j);
+                if (count(mat[i].begin() , mat[i].end() , 1) > 1)
+                    break;
+                if (count(revMat[j].begin() , revMat[j].end() , 1) > 1)
+                    break;
+                
+                else {
+                    ans ++;
+                    break;
+                }
             }
         }
     }
