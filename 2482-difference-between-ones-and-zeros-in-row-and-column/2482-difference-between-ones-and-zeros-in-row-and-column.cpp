fa756bb54c11996da287bfd0204ed86960f84eaa
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-12-14 08:33:22 +0200

    Time: 239 ms (20.84%), Space: 136.7 MB (22.24%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> onesMinusZeros(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size();
+    vector<vector<int>> onesMinusZeros(vector<vector<int>>& grid) {int n = grid.size(), m = grid[0].size();
 
     vector <vector<int>> diff(n , vector<int>(m));
     vector <int> oneRow(n) , oneColumn(m);
     vector <int> zeroRow(n) , zeroColumn(m);
     for (int i = 0;i < n; i++){
         oneRow[i] = count(grid[i].begin() , grid[i].end() , 1);
-        zeroRow[i] = count(grid[i].begin() , grid[i].end() , 0);
+        zeroRow[i] = n - oneRow[i];
     }
 
     vector <vector<int>> rev(m , vector <int> (n));
 
     for (int i = 0;i < m;i ++){
+        int sumOne = 0;
         for (int j = 0; j < n;j ++){
             rev[i][j] = grid[j][i];
+            sumOne += rev[i][j];
         }
 
         oneColumn[i] = count(rev[i].begin() , rev[i].end() , 1);
-        zeroColumn[i] = count(rev[i].begin() , rev[i].end() , 0);
+        zeroColumn[i] = m - sumOne;
 
     }
 
