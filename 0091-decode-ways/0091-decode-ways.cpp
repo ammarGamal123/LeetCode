d82a4489c29b9f6a933ec91497b43ea955ab3c57
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-12-25 22:58:45 +0200

    Time: 0 ms (100.00%), Space: 6.5 MB (65.60%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector <int> dp;
+
+    int numDecodings(string s) {
+        if (s.front() == '0') return 0;
+        
+        dp.resize(s.size() + 1 , -1);
+        
+        return solve (s , 0);
+    }
+    int solve (string &s , int i){
+        if (i >= s.size()){
+            return 1;
+        }
+        if (dp[i] != -1) return dp[i];
+        
+    
+        if (s[i] == '0')
+            return dp[i] = 0;
+    
+       int ans = solve (s , i + 1);
+    
+        if (i + 1 < s.size() && (s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6'))){
+            ans += solve (s , i + 2);
+        }
+        
+        return dp[i] = ans;
+    }
+};
