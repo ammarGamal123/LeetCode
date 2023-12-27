1c77501e69ef883898d5ec851ce87306404e37b5
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2023-12-27 16:33:54 +0200

    Time: 115 ms (70.49%), Space: 96 MB (26.97%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -6,16 +6,15 @@ public:
      for (int i = 1; i < colors.size(); i++) {
          if (colors[i] == colors[i - 1]) {
              int j = i , sum = neededTime[i - 1];
-             priority_queue <int , vector<int> , less<>> pq;
-             pq.push(neededTime[i - 1]);
+             int maxTime = neededTime[i - 1];
              while (j < colors.size() && colors[i - 1] == colors[j]){
                  sum += neededTime[j];
-                 pq.push(neededTime[j ++]);
+                 maxTime = max (maxTime , neededTime[j ++]);
              }
              if (j < colors.size()) i = j;
              else i = j - 1;
            //  cout << sum << " " << pq.top() << endl;
-             ans += sum - pq.top();
+             ans += sum - maxTime;
          }
      }
 
