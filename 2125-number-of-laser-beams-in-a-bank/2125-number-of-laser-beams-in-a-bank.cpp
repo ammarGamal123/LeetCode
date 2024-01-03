2748120d1737410a8493ab296df9d5e6ac7ffebe
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2024-01-03 20:00:24 +0200

    Time: 82 ms (85.04%), Space: 29.3 MB (24.35%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        long long ans {};
+       
+        int prev = 0 , ans = 0;
         
-        int start = -1;
-        for (int i = 0;i < bank.size(); i++){
-            if (count(bank[i].begin() , bank[i].end() , '1')){
-                start = i;
-                break;
+        for (string s : bank){
+            int count = 0;
+            
+            for (char c : s)
+                count += c == '1';
+            
+            if (count){
+                ans += (prev * count) , prev = count;
             }
         }
-        if (start == -1) return 0;
-        int prevDevices = count(bank[start].begin() , bank[start].end() , '1');
-        
-        for (int i = start + 1; i < bank.size(); i++){
-            
-            int curDevices = count(bank[i].begin() , bank[i].end() , '1');
-            
-            if (!curDevices) continue;
-            
-            ans += (curDevices * prevDevices);
-            prevDevices = curDevices;
-        }
-                                         
         return ans;
     }
 };
