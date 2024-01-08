4375f74476fcd94c406ddca59cf1a2435f27f754
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2024-01-08 20:42:57 +0200

    Time: 150 ms (11.58%), Space: 78.3 MB (5.36%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -11,19 +11,19 @@
  */
 class Solution {
 public:
-    void traceBST (TreeNode* root , unordered_map <int,int> &mp){
+    void traceBST (TreeNode* root , unordered_map <int,bool> &mp){
         if (root == NULL) return;
         
-        mp[root->val] ++;
+        mp[root->val] = true;
         traceBST(root->left , mp);
         
-        mp[root->val] ++;
+        mp[root->val] = true;
         traceBST(root->right , mp);
         
        
     }
     int rangeSumBST(TreeNode* root, int low, int high) {
-        unordered_map <int,int> mp;
+        unordered_map <int,bool> mp;
         
         traceBST(root , mp);
         
