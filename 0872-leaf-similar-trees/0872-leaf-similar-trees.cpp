42b9fe8d2f051883a31f262b086209ee60362ae8
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2024-01-10 00:06:28 +0200

    Time: 0 ms (100.00%), Space: 13.2 MB (49.43%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -24,26 +24,12 @@ public:
         
         leafSeq1(root->right , st1);
     }
-    
-     void leafSeq2(TreeNode* root , vector <int> &st2){
-        if (root == NULL){
-            return ;
-        }
-        
-        if (root->left == NULL && root->right == NULL){
-            st2.emplace_back(root->val);
-        }
-        
-        leafSeq2(root->left , st2);
-        
-        
-        leafSeq2(root->right , st2);
-    }
+
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
        vector<int> st1 , st2;
         
         leafSeq1(root1 , st1);
-        leafSeq2(root2 , st2);
+        leafSeq1(root2 , st2);
         
         return st1 == st2;
     }
