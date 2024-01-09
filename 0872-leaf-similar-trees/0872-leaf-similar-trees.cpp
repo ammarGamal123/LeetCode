93eff8aa55619d569d18425d8f9a64f525d9287c
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2024-01-09 23:56:17 +0200

    Time: 7 ms (20.21%), Space: 13.1 MB (96.01%) - LeetHub

---
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void leafSeq1(TreeNode* root , stack <int> &st1){
+        if (root == NULL){
+            return ;
+        }
+        
+        if (root->left == NULL && root->right == NULL){
+            st1.push(root->val);
+        }
+        
+        leafSeq1(root->left , st1);
+        
+        leafSeq1(root->right , st1);
+    }
+    
+     void leafSeq2(TreeNode* root , stack <int> &st2){
+        if (root == NULL){
+            return ;
+        }
+        
+        if (root->left == NULL && root->right == NULL){
+            st2.push(root->val);
+        }
+        
+        leafSeq2(root->left , st2);
+        
+        
+        leafSeq2(root->right , st2);
+    }
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+       stack <int> st1 , st2;
+        
+        leafSeq1(root1 , st1);
+        leafSeq2(root2 , st2);
+        
+        return st1 == st2;
+    }
+};
