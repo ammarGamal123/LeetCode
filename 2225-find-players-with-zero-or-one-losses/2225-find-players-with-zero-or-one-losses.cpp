6a37319798b72587929145176706a7430c66c085
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2024-01-16 02:08:02 +0200

    Time: 744 ms (17.58%), Space: 180.3 MB (39.96%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -1,22 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-       set<int>all;
-       map<int,int>loser;
-       for (auto i : matches)
-       {
-           loser[i[1]]++; 
-           all.insert(i[0]); 
-           all.insert(i[1]); 
-       }
-       vector<vector<int>>ans(2); 
-       for (auto i : all)
-       {
-           if (loser[i]==1)
-               ans[1].push_back(i); 
-           else if (loser[i]==0)
-               ans[0].push_back(i); 
-       }
-       return ans; 
+        vector <vector<int>> ans(2);
+        
+        vector <bool> vis1(100005) , vis2(100005);
+        multiset <int> st2;
+        set <int> st1;
+        
+        
+        for (int i = 0;i < matches.size(); i++){
+            
+            int win = matches[i][0] , los = matches[i][1];
+            
+            st1.insert(win) , st2.insert(los);
+            vis1[win] = true , vis2[los] = true;
+        }
+        
+        for (int i = 0;i < matches.size(); i++){
+            
+            int win = matches[i][0] , los = matches[i][1];
+            
+            if (vis2[win]){
+                st1.erase(win);
+                vis2[win] = false;
+            }
+            
+            if (st2.count(los) > 1){
+                st2.erase(los);
+            }
+        }
+        
+        for (auto i : st1) ans[0].emplace_back(i);
+        
+        for (auto i : st2) ans[1].emplace_back(i);
+        
+        return ans;
     }
 };
