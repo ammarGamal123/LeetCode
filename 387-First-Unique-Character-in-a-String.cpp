c1b4f8558abb871eb50f4d5d90aedf30e8d931e8
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2024-02-05 17:10:06 +0200

    [387] [Time Beats: 10.77%] [Memory Beats: 7.85%] - LeetPush

--- 387-First-Unique-Character-in-a-String.cpp
+++ 387-First-Unique-Character-in-a-String.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        vector <int> freq(26);
+        map <char,int> freq;
         for (char c : s)
-            freq[c - 'a'] ++;
+            freq[c] ++;
 
         for (int i = 0; i < s.size(); i++){
-            if (freq[s[i] - 'a'] == 1)
+            if (freq[s[i]] == 1)
                 return i;
         }
         return -1;
