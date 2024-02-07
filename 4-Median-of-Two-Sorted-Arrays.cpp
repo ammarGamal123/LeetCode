0f428304f1a2a80a1148ff57113a4ffe0970cc4e
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2024-02-07 18:02:43 +0200

    [4] [Time Beats: 24.01%] [Memory Beats: 58.00%] - LeetPush

--- 4-Median-of-Two-Sorted-Arrays.cpp
+++ 4-Median-of-Two-Sorted-Arrays.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
+    int p1 = 0 , p2 = 0;
+    int getMin (vector <int>& a1 , vector<int>& a2){
+        if (p1 < a1.size() && p2 < a2.size()){
+            return a1[p1] < a2[p2] ? a1[p1 ++]: a2[p2 ++]; 
+        }
+        else if (p1 < a1.size()){
+            return a1[p1 ++];
+        }
+        else if (p2 < a2.size()){
+            return a2[p2 ++];
+        }
+        return -1;
+    }
+    
     double findMedianSortedArrays(vector<int>& a1, vector<int>& a2) {
-        vector <int> merged;
-        for (int i = 0;i < a1.size(); i++){
-            merged.emplace_back(a1[i]);
-        }
-        for (int i = 0;i < a2.size(); i++){
-            merged.emplace_back(a2[i]);
-        }
-        sort (merged.begin() , merged.end());
+       int m = a1.size() , n = a2.size();
+       if ((m + n) % 2 == 0){
+           for (int  i = 0;i < (m + n) / 2 - 1; i++){
+               int _ = getMin(a1 , a2);
+           }
+           return (double)(getMin(a1 , a2) + getMin(a1 , a2)) / 2;
+       }
+       else {
+           for (int i = 0;i < (m + n) / 2; i++){
+               int _ = getMin(a1 , a2);
+           }
+           return getMin(a1 , a2);
+       }
 
-        double median = 0;
-
-        int n = merged.size() / 2 , sz = merged.size();
-        if (sz % 2 == 0){
-            median = (merged[n] + merged[n - 1]) / 2.0;
-        }
-        else median = merged[n];
-
-        return (median / 1.0);
+       return -1;
 
     }
 };
