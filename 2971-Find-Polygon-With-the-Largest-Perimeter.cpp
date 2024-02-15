f86108f0b3982440fdb352917ac288fe2608f048
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2024-02-16 00:13:33 +0200

    [2971] [Time Beats: 35.31%] [Memory Beats: 33.29%] - LeetPush

--- 2971-Find-Polygon-With-the-Largest-Perimeter.cpp
+++ 2971-Find-Polygon-With-the-Largest-Perimeter.cpp
@@ -3,8 +3,8 @@ public:
     long long largestPerimeter(vector<int>& a) {
         long long ans {-1};
         sort (a.begin() , a.end());
-        long prevSum {};
-        for (int i = 0;i < a.size(); i++){
+        long prevSum {a[0] + a[1]};
+        for (int i = 2;i < a.size(); i++){
             if (a[i] < prevSum && i >= 2){
                 ans = a[i] + prevSum;
             }
