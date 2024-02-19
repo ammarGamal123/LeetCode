3f80f6b5c0ceec23893e4214f97859340bb54ad8
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2024-02-19 13:36:38 +0200

    [231] [Time Beats: 19.05%] [Memory Beats: 100.00%] - LeetPush

---
+++ 231-Power-of-Two.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n == 1) return true;
+        
+        if (n & 1 || !n) return false;
+        
+        return isPowerOfTwo(n / 2);
+    }
+};
