824814534a7f1b5f84a5ea1f47e8d8b603a8024c
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2024-03-06 00:04:21 +0200

    Time: 23 ms (76.06%), Space: 11.8 MB (73.98%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
     int bestClosingTime(string customers) {
-        int n=customers.length();
+        int n = customers.size() , penalty {} , maxPenalty {} , index {-1};
         
-		// Storing count of 'Y' penalties on right if we close on 'ith index'
-        vector<int> right(n+1,0);
-        for(int i=n-1;i>=0;i--) right[i] = (i<n-1 ? right[i+1]:0) + (customers[i]=='Y');
-        
-		 // storing 'N' penalties on left if close on 'ith' index
-        vector<int> left(n,0);
-        
-        int min_penalty=n+1;
-        int idx=-1;
-        
-        for(int i=0;i<n;i++){
-            int penalty = right[i] + (i>0 ? left[i-1]:0);
+        for (int i = 0;i < n; i++){
+            penalty += customers[i] == 'Y' ? 1 : -1;
             
-            if(min_penalty > penalty){
-                min_penalty=min(min_penalty,penalty);
-                idx = i;
+            if (penalty > maxPenalty){
+                maxPenalty = penalty;
+                index = i;
             }
-			
-			left[i] = (i>0 ? left[i-1]:0) + (customers[i]=='N');
         }
         
-		// if we decide to close after all customers have come
-        if(min_penalty > right[n] + left[n-1]) return n;
-		
-        return idx;
+        return index + 1;
     }
 };
