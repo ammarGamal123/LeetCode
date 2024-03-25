b3812e9ca7b788d1932e9ec47d8f6a9869584c00
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2024-03-25 22:45:48 +0200

    Time: 80 ms (18.33%), Space: 48.1 MB (28.87%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -3,12 +3,12 @@ public:
     vector<int> findDuplicates(vector<int>& a) {
                 
         vector <int> ans;
-        map <int,int> mp;
+        unordered_map <int,int> mp;
         
         for (int i = 0;i < a.size(); i++){
             ++ mp[a[i]];
             if (mp[a[i]] > 1)
-                ans.emplace_back(a[i]) , mp.erase(a[i]);
+                ans.emplace_back(a[i]) , mp[a[i]] = 0;
         }
         
        
