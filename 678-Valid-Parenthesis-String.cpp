2881f5a4adfa11b80ef9b2cba4f505b3e179c395
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2024-04-08 01:30:40 +0200

    [678] [Time Beats: 45.67%] [Memory Beats: 39.42%] - LeetPush

--- 678-Valid-Parenthesis-String.cpp
+++ 678-Valid-Parenthesis-String.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        stack<pair<char,int>> st;
-    short stars{};
-    priority_queue <short> pq;
-    for (short i = 0; i < s.size(); i++) {
-        if (s[i] == '*')
-            ++stars,pq.push(i);
-        else if (st.empty() && !stars && s[i] == ')')
-            return false;
-        else if (!st.empty() && st.top().first == '(' && s[i] == ')')
-            st.pop();
-        else if (stars && s[i] == ')')
-            --stars , pq.pop();
-        else if (s[i] == '(')
-            st.push({s[i] , i});
-    }
-    
-    if (st.empty() && pq.empty()) return true;
-    else if (!st.empty() && pq.empty()) return false;
-    else if (st.size() > pq.size()) return false;
-    while (st.size() && pq.size()){
-        if (st.top().second > pq.top())
-            return false;
-        else
-            pq.pop() , st.pop();
-    }
-  //  cout << st.size() << \ \  << stars << endl;
-    return true;
+        stack<pair<char,short>> st;
+        short stars{};
+        vector <int> idx;
+        for (short i = 0; i < s.size(); i++) {
+            if (s[i] == '*')
+                ++stars , idx.emplace_back(i);
+            else if (st.empty() && !stars && s[i] == ')')
+                return false;
+            else if (!st.empty() && st.top().first == '(' && s[i] == ')')
+                st.pop();
+            else if (stars && s[i] == ')')
+                --stars , idx.pop_back();
+            else if (s[i] == '(')
+                st.push({s[i] , i});
+        }
+        
+        if (st.empty() && idx.empty()) return true;
+        else if (!st.empty() && idx.empty()) return false;
+        else if (st.size() > idx.size()) return false;
+        while (st.size() && idx.size()){
+            if (st.top().second > idx.back())
+                return false;
+            else
+                idx.pop_back() , st.pop();
+        }
+    //  cout << st.size() << \ \  << stars << endl;
+        return true;
     }
 };
