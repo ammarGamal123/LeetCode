44d664d386e9d7aa27f3bb931571a68af827a2ef
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2024-04-19 17:41:40 +0200

    [42] [Time Beats: 100.00%] [Memory Beats: 82.39%] - LeetPush

--- 42-Trapping-Rain-Water.cpp
+++ 42-Trapping-Rain-Water.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int n = height.size();
-        if (n == 0) return 0;
-        
-        vector<int> left(n);
-        vector<int> right(n);
-        
-        // Fill left array
-        left[0] = height[0];
-        for (int i = 1; i < n; i++) {
-            left[i] = max(left[i - 1], height[i]);
+    int trap(std::vector<int>& height) {
+        int i = 0, left_max = height[0], sum = 0;
+        int j = height.size() - 1, right_max = height[j];
+        while (i < j) {
+            if (left_max <= right_max) {
+                sum += (left_max - height[i]);
+                i++;
+                left_max = std::max(left_max, height[i]);
+            } else {
+                sum += (right_max - height[j]);
+                j--;
+                right_max = std::max(right_max, height[j]);
+            }
         }
-        
-        // Fill right array
-        right[n - 1] = height[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            right[i] = max(right[i + 1], height[i]);
-        }
-        
-        // Calculate trapped water
-        int trappedWater = 0;
-        for (int i = 0; i < n; i++) {
-            trappedWater += min(left[i], right[i]) - height[i];
-        }
-        
-        return trappedWater;
+        return sum;
     }
 };
