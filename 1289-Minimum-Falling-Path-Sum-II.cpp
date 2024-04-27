ef5b237e3d220914b062c60b8042686a8d7b2d3b
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2024-04-28 00:38:17 +0300

    [1289] [Time Beats: 62.41%] [Memory Beats: 94.23%] - LeetPush

--- 1289-Minimum-Falling-Path-Sum-II.cpp
+++ 1289-Minimum-Falling-Path-Sum-II.cpp
@@ -1,37 +1,24 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& grid) {
-        // Initialize a two-dimensional array to cache the result of each sub-problem
-        vector<vector<int>> memo(grid.size(), vector<int>(grid.size(), INT_MAX));
 
-        // Fill the base case
-        for (int col = 0; col < grid.size(); col++) {
-            memo[grid.size() - 1][col] = grid[grid.size() - 1][col];
-        }
+    for (int i = 1; i < grid.size(); i++){
+        for (int j = 0; j < grid[i].size(); j++){
+            int a = INT_MAX;
 
-        // Fill the recursive cases
-        for (int row = grid.size() - 2; row >= 0; row--) {
-            for (int col = 0; col < grid.size(); col++) {
-                // Select minimum from valid cells of next row
-                int nextMinimum = INT_MAX;
-                for (int nextRowCol = 0; nextRowCol < grid.size(); nextRowCol++) {
-                    if (nextRowCol != col) {
-                        nextMinimum = min(nextMinimum, memo[row + 1][nextRowCol]);
-                    }
-                }
-
-                // Minimum cost from this cell
-                memo[row][col] = grid[row][col] + nextMinimum;
+            for (int k = 0; k < grid[i].size(); k++){
+                if (k != j)
+                    a = min (grid[i - 1][k] , a);
             }
+            grid[i][j] += a;
         }
+    }
 
-        // Find the minimum from the first row
-        int answer = INT_MAX;
-        for (int col = 0; col < grid.size(); col++) {
-            answer = min(answer, memo[0][col]);
-        }
+    int ans = INT_MAX;
+    for (int i : grid[grid.size() -1])
+        ans = min(ans , i);
 
-        // Return the answer
-        return answer;
+    return ans;
+
     }
 };
