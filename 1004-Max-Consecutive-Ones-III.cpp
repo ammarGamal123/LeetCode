5b870ea320b3d59df9c527e33140c22ecaacc599
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2024-08-19 22:36:15 +0300

    [1004] [Time Beats: 61.80%] [Memory Beats: 54.94%] - LeetPush

--- 1004-Max-Consecutive-Ones-III.cpp
+++ 1004-Max-Consecutive-Ones-III.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     int longestOnes(vector<int>& a, int k) {
-    if (!count(a.begin() , a.end() , 1))
-        return k;
+    int left = 0 , right = 0 , n = a.size();
+    int maxOnes = 0;
 
-    int n = a.size();
-    int maxOnes {} , leftZeroPosition {};
-    deque<int> zeroPos;
+    while (right < n){
+       if (!a[right])
+           -- k;
 
-    for (int i = 0;i < n; i++){
-        if (!a[i])
-            zeroPos.push_back(i);
-
-        if (zeroPos.size() > k){
-            leftZeroPosition = zeroPos.front() + 1;
-            zeroPos.pop_front();
-        }
-
-        maxOnes = max(maxOnes , i - leftZeroPosition + 1);
+       while (k < 0){
+            if (!a[left]){
+                ++ k;
+            }
+            ++ left;
+       }
+       maxOnes = max(maxOnes , right - left + 1);
+       ++ right;
     }
-
-    return  maxOnes;
+    return maxOnes;
 }
 };
