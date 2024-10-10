cb1467dbc99a75a0cac921ee82621a5a015881f0
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2024-10-10 12:44:42 +0300

    [3309] [Time Beats: 50.26%] [Memory Beats: 35.79%] - LeetPush

--- 3309-Maximum-Possible-Number-by-Binary-Concatenation.cpp
+++ 3309-Maximum-Possible-Number-by-Binary-Concatenation.cpp
@@ -10,26 +10,23 @@ public:
     return binary;
 }
 
-int to_int(string s) {
-    int res=0;
-    for(char c : s) {
-        res=res*2+c-'0';
-    }
-    return res;
-}
-
 int maxGoodNumber(vector<int>& a) {
     int n = a.size();
     string str1 = toBinary(a[0]);
     string str2 = toBinary(a[1]);
     string str3 = toBinary(a[2]);
 
-    vector <string> s{str1 , str2 , str3};
-    sort(s.begin() , s.end());
-    int mx=0;
-        do {
-            mx=max(mx,to_int(s[0]+s[1]+s[2]));
-        } while(next_permutation(s.begin() , s.end()));
-        return mx;
+    vector <string> binaries{str1 , str2 , str3};
+    int maxBinary = 0;
+    sort (binaries.begin() , binaries.end());
+    do {
+        string concatonate = binaries[0] + binaries[1] + binaries[2];
+
+        maxBinary = max(maxBinary , 
+                        stoi(concatonate , nullptr , 2));
+    } while(next_permutation(binaries.begin() , binaries.end()));
+
+    // convert from binary string to int
+    return maxBinary;
 }
 };
