cc05e4ef7d8d3226ddb27dcf01ca0b76281fc988
Ammar Hammad <100167068+ammarGamal123@users.noreply.github.com>
2024-10-10 11:18:52 +0300

    [962] [Time Beats: 12.28%] [Memory Beats: 8.90%] - LeetPush

--- 962-Maximum-Width-Ramp.cpp
+++ 962-Maximum-Width-Ramp.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-int maxWidthRamp(vector<int>&a){
-    int n = a.size();
-    stack <int> st;
-
-    // build the stack with decreasing indices
+int maxWidthRamp(vector<int>& a) {
+    int n = a.size() , maxWidth {};
+    vector <int> indecies(n);
     for (int i = 0; i < n; i++) {
-        if (st.empty() || a[st.top()] > a[i]) {
-            st.push(i);
-        }
+        indecies[i] = i;
     }
+    sort (indecies.begin(), indecies.end(),[&](int i , int j) {
+        return a[i] != a[j] ? a[i] < a[j] : i < j;
+    });
 
-    int maxWidth = 0;
 
-    for (int j = n - 1; j >= 0; j--) {
-        while (!st.empty() && a[st.top()] <= a[j]) {
-            maxWidth = max(maxWidth , j - st.top());
-            st.pop();
-        }
+
+    int minIndex = n;
+    for (int idx : indecies) {
+        maxWidth = max(maxWidth , idx - minIndex);
+        minIndex = min(minIndex, idx);
     }
 
     return maxWidth;
